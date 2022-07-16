6e2e7335601053974f949840aba159d9f2f05407
ludwig300 <105148929+ludwig300@users.noreply.github.com>
2022-07-16 19:41:17 +0700

    Add publish photo to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -9,9 +9,16 @@ def public_text(chat_id, text):
     bot.send_message(chat_id=chat_id, text=text)
 
 
+def public_image(chat_id, path):
+    token = os.environ['TOKEN']
+    bot = telegram.Bot(token)
+    bot.send_document(chat_id=chat_id, document=open(path, 'rb'))
+
+
 def main():
     load_dotenv()
-    public_text('@testchanal0', 'Hello World')
+    # public_text('@testchanal0', 'Hello World')
+    public_image('@testchanal0', 'image/nasa_apod_0.jpg')
 
 
 if __name__ == '__main__':
