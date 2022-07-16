c9ec22fee2de6453c2474fd52eb94e01b7b9b6a5
ludwig300 <105148929+ludwig300@users.noreply.github.com>
2022-07-16 18:23:03 +0700

    Update  fetch_spacex_images.py

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -1,24 +1,9 @@
 import requests
 import argparse
-import urllib.parse
-import os
 from dotenv import load_dotenv
 from pathlib import Path
-
-
-def download_image(url, path):
-    response = requests.get(url)
-    response.raise_for_status()
-    with open(path, 'wb') as file:
-        file.write(response.content)
-
-
-def get_extension(urlstring):
-    parsed_url = urllib.parse.urlsplit(urlstring, scheme='', allow_fragments=True)
-    unquoted_filename = urllib.parse.unquote(parsed_url[2], encoding='utf-8', errors='replace')
-    splited_filename = os.path.split(unquoted_filename)
-    extension = os.path.splitext(splited_filename[1])
-    return extension[1]
+from download_image import download_image
+from extension import get_extension
 
 
 def fetch_spacex_last_launch(id):
