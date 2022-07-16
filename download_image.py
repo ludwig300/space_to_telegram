f3413f017a911fe99fb57f9694d54db25b6d5144
ludwig300 <105148929+ludwig300@users.noreply.github.com>
2022-07-16 18:21:21 +0700

    Moved the script download_image

---
+++ download_image.py
@@ -0,0 +1,10 @@
+import requests
+
+
+def download_image(url, path):
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(path, 'wb') as file:
+        file.write(response.content)
+
+
