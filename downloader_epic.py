859948948bb605cee17cac87349d76ced2602428
ludwig300 <105148929+ludwig300@users.noreply.github.com>
2022-07-21 19:53:11 +0700

    Added argparse

--- downloader_epic.py
+++ downloader_epic.py
@@ -4,16 +4,17 @@ import os
 import datetime
 from dotenv import load_dotenv
 from downloader import download_image
+import argparse
 
 
-def get_epic_image(nasa_api_key):
+def get_epic_image(nasa_api_key, count_images):
     payload = {
-        'api_key': f'{nasa_api_key}',   
+        'api_key': f'{nasa_api_key}',
     }
     url_api_epic = 'https://api.nasa.gov/EPIC/api/natural'
     response = requests.get(url_api_epic, params=payload)
     response.raise_for_status()
-    for number_epic in range(5):
+    for number_epic in range(count_images):
         date = datetime.datetime.fromisoformat(response.json()[number_epic]['date'])
         formatted_date = date.strftime("%Y/%m/%d")
         image_name = response.json()[number_epic]['image']
@@ -23,12 +24,20 @@ def get_epic_image(nasa_api_key):
         download_image(response_new.url, path)
 
 
+def createParser ():
+    parser = argparse.ArgumentParser(description='Downloader APOD images from NASA')
+    parser.add_argument ('-c', '--count', default=1, type=int, help='Count images to download. Default download 1 image.')
+    return parser
+
+
 def main():
     load_dotenv()
+    parser = createParser()
+    namespace = parser.parse_args()
     nasa_api_key = os.environ['NASA_API']
     Path("image").mkdir(parents=True, exist_ok=True)
-    get_epic_image(nasa_api_key)
-    
+    get_epic_image(nasa_api_key, namespace.count)
+
 
 if __name__ == '__main__':
     main()
